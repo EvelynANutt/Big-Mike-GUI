301f50b52ade0508bc2c7edb1352d14486e1d59f
EvelynANutt <evelynan@stanford.edu>
2025-02-18 01:42:26 -0800

    an even better demo

--- src/business/attocube.py
+++ src/business/attocube.py
@@ -14,6 +14,12 @@ class Attocube:
     def __init__(self):
         self.device = AMC.Device('192.168.1.1')
         self.device.connect()
+        self.device.control.setControlOutput(0, True)
+        self.device.control.setControlOutput(1, True)
+        self.device.control.setControlOutput(2, True)
+        self.device.control.setControlMove(0, True)
+        self.device.control.setControlMove(1, True)
+        self.device.control.setControlMove(2, True)
 
     def get_value(self, dim: int) -> float:
         value = self.device.control.getPositionsAndVoltages()[dim]
@@ -24,3 +30,11 @@ class Attocube:
 
     def set_value(self, dim: int, new_value: float):
         self.device.move.setControlTargetPosition(dim, new_value)
+
+    def __del__(self):
+        self.device.control.setControlOutput(0, False)
+        self.device.control.setControlOutput(1, False)
+        self.device.control.setControlOutput(2, False)
+        self.device.control.setControlMove(0, False)
+        self.device.control.setControlMove(1, False)
+        self.device.control.setControlMove(2, False)
