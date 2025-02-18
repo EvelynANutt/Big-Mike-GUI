b9e991a8ad1c426c9a9d78ef0ae0af43e6002b6e
EvelynANutt <evelynan@stanford.edu>
2025-02-17 19:41:00 -0800

    Increment stage fixed

--- src/business/attocube.py
+++ src/business/attocube.py
@@ -4,10 +4,14 @@ from typing import Callable
 import PySpin
 import time
 from . import AMC
+import time
 
 # axis: [0|1|2] == [x|y|z]
 #these variables are for the xy positions of the upper left corner and lower right corner
 fx1,fy1,fx2,fy2 = 740,713,942,793
+
+# Global variable for stage movement: Set to 1000nm = 1um
+scale = 1000
 
 class Attocube:
     def __init__(self):
@@ -29,39 +33,54 @@ class Attocube:
 
     def increment_up(self, dim):
         if (dim == 0):
-            new_x = self.xpos + 1
+            new_x = self.xpos + scale
+            time.sleep(1)
             self.device.move.setControlTargetPosition(0, new_x)
+            time.sleep(1)
             self.xpos = self.device.control.getPositionsAndVoltages()[0]
         if (dim == 1):
-            new_y = self.ypos + 1
+            new_y = self.ypos + scale
+            time.sleep(1)
             self.device.move.setControlTargetPosition(1, new_y)
+            time.sleep(1)
             self.ypos = self.device.control.getPositionsAndVoltages()[1]
         if (dim == 2):
-            new_z = self.zpos + 1
+            new_z = self.zpos + scale
+            time.sleep(1)
             self.device.move.setControlTargetPosition(2, new_z)
+            time.sleep(1)
             self.zpos = self.device.control.getPositionsAndVoltages()[2]   
 
     def increment_down(self, dim):
         if (dim == 0):
-            new_x = self.xpos - 1
+            new_x = self.xpos - scale
+            time.sleep(1)
             self.device.move.setControlTargetPosition(0, new_x)
+            time.sleep(1)
             self.xpos = self.device.control.getPositionsAndVoltages()[0]
         if (dim == 1):
-            new_y = self.ypos - 1
+            new_y = self.ypos - scale
+            time.sleep(1)
             self.device.move.setControlTargetPosition(1, new_y)
+            time.sleep(1)
             self.ypos = self.device.control.getPositionsAndVoltages()[1]
         if (dim == 2):
-            new_z = self.zpos - 1
+            new_z = self.zpos - scale
+            time.sleep(1)
             self.device.move.setControlTargetPosition(2, new_z) 
+            time.sleep(1)
             self.zpos = self.device.control.getPositionsAndVoltages()[2]      
 
     def move(self, dim, absolute_pos):
         if (dim == 0):
             self.device.move.setControlTargetPosition(0, absolute_pos)
+            time.sleep(1)
             self.xpos = self.device.control.getPositionsAndVoltages()[0]
         if (dim == 1):
             self.device.move.setControlTargetPosition(1, absolute_pos)
+            time.sleep(1)
             self.ypos = self.device.control.getPositionsAndVoltages()[1]
         if (dim == 2):
             self.device.move.setControlTargetPosition(2, absolute_pos)
+            time.sleep(1)
             self.zpos = self.device.control.getPositionsAndVoltages()[2]
