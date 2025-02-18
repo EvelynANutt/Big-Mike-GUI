24ce361e85ed29c2fd258093f7d06f6eca83cc19
EvelynANutt <evelynan@stanford.edu>
2025-02-17 19:28:54 -0800

    Lambda for function NoneType fix attempt

--- src/obs_deck/control_panel/stage_controls.py
+++ src/obs_deck/control_panel/stage_controls.py
@@ -18,12 +18,18 @@ class StageControlPanel:
         self.title = tk.Label(self.header, text='Stage Controls', font=('Aria',26))
 
         # xyz translation and xy cropping names
-        self.x_trans_input = NumberInput(self.frame, title='x [um]', command_set_up=self.store.attocube.increment_up(0), 
-                                         command_set_down=self.store.attocube.increment_down(0), command_set_abs=self.store.attocube.move(0,0))
-        self.y_trans_input = NumberInput(self.frame, title='y [um]', command_set_up=self.store.attocube.increment_up(1), 
-                                         command_set_down=self.store.attocube.increment_down(1), command_set_abs=self.store.attocube.move(1,0))
-        self.z_trans_input = NumberInput(self.frame, title='z [um]', command_set_up=self.store.attocube.increment_up(2), 
-                                         command_set_down=self.store.attocube.increment_down(2), command_set_abs=self.store.attocube.move(2,0))
+        self.x_trans_input = NumberInput(self.frame, title='x [um]', 
+                                         command_set_up=lambda: self.store.attocube.increment_up(0), 
+                                         command_set_down=lambda: self.store.attocube.increment_down(0), 
+                                         command_set_abs=lambda: self.store.attocube.move(0,0))
+        self.y_trans_input = NumberInput(self.frame, title='y [um]', 
+                                         command_set_up=lambda: self.store.attocube.increment_up(1), 
+                                         command_set_down=lambda: self.store.attocube.increment_down(1), 
+                                         command_set_abs=lambda: self.store.attocube.move(1,0))
+        self.z_trans_input = NumberInput(self.frame, title='z [um]', 
+                                         command_set_up=lambda: self.store.attocube.increment_up(2), 
+                                         command_set_down=lambda: self.store.attocube.increment_down(2), 
+                                         command_set_abs=lambda: self.store.attocube.move(2,0))
 
     def render(self):
         self.title.pack(side='left', expand=True)
